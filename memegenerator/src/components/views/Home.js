1ebf9fd082fa6ae697f8c92f840d8dbb1a8fbfd7
Jigar <shahjigar185@gmail.com>
2020-10-02 10:03:24 +0530

    Fetched Data from Api

--- memegenerator/src/components/views/Home.js
+++ memegenerator/src/components/views/Home.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import {useState,useEffect} from 'react'
+import axios from 'axios';
+
 
 export default function Home() {
+    const [memedata,setMemeData]=useState([]);
+    console.log(memedata);
+    
+    //Getting data from API
+    const sendGetRequest=async()=>{
+      try{
+        const resp=await axios.get('https://api.imgflip.com/get_memes')
+        console.log(resp.data)
+        setMemeData(resp.data)
+      }catch(e){
+          console.log("Error in Fetching Data")
+      }
+    }
+    useEffect(()=>{
+       sendGetRequest();
+    },[])
     return (
         <div>
-            <h1>Home</h1>
+            <h1>DashBoard For memes</h1>
         </div>
     )
 }
